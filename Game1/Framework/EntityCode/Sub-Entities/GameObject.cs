13ee08632c5d4d7e6bd6f838259dc8fdbbd4342d
Finn-Gale <41002558+Finn-Gale@users.noreply.github.com>
2019-03-18 12:56:07 +0000

    Began some work on sterring behaviours

--- Game1/Framework/EntityCode/Sub-Entities/GameObject.cs
+++ Game1/Framework/EntityCode/Sub-Entities/GameObject.cs
@@ -42,6 +42,23 @@ namespace Game1.Framework.EntityCode.Sub_Entities
 
         protected Boolean _CollideR;
 
+
+        //realistic movment code
+
+        //DECLAREsome physics instance variables
+
+        //Value for the mass of the Gameobject 
+        protected float _mMass;
+
+        //a value for the acceleration
+        protected Vector2 _mAccel = new Vector2(0, 0);
+
+        //a value for the  friction of the gameobject
+        protected float _mFriction;
+
+        //declare a value for force affecting an object
+        protected Vector2 _mForce;
+
         public GameObject()
         {
 
@@ -138,6 +155,24 @@ namespace Game1.Framework.EntityCode.Sub_Entities
 
         public virtual void CollReact(Vector2 pMTV)
         {
+
+        }
+
+        //The purpose of this method is to move the game object in a realistic manner
+        protected void Move(Vector2 pForce)
+        {
+            //Set the Acceleration of game object based on the force being applied to the gameobject
+            _mAccel.X = pForce.X / _mMass;
+            _mAccel.Y = pForce.Y / _mMass;
+
+            //Addjust the veloctiy of an object by appling the acceleration
+
+            _Velocity.X = _Velocity.X + _mAccel.X;
+            _Velocity.Y = _Velocity.Y + _mAccel.Y;
+
+            //update the position of the Game object based on velocity
+            _Position.X = _Position.X + _Velocity.X;
+            _Position.Y = _Position.Y + _Velocity.Y;
 
         }
         #region Properties
